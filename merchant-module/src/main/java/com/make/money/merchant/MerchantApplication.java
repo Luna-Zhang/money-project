76f928f9f812834da261d7bf6f23587639796dff
Luna-Zhang <zss20140625@163.com>
2021-11-25 19:10:01 +0800

    优化

--- merchant-module/src/main/java/com/make/money/merchant/MerchantApplication.java
+++ merchant-module/src/main/java/com/make/money/merchant/MerchantApplication.java
@@ -1,9 +1,11 @@
 package com.make.money.merchant;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.make.money")
+@MapperScan(basePackages = {"com.make.money.data.mapper"})
 public class MerchantApplication {
 
     public static void main(String[] args) {
